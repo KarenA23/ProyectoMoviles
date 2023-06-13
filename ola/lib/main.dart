a36b211b354de4c24565f75493e4546d292757e0
KarenA23 <karennahomy23@gmail.com>
2023-06-13 09:39:19 -0500

    fee

--- ola/lib/main.dart
+++ ola/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:________________-kdkdk',
+              'You have pushed the button this many times:________________-',
             ),
             Text(
               '$_counter',
